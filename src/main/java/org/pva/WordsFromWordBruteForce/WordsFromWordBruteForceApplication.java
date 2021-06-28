9f1f64157c1530abf7772de1991439e43b7a3093
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-06-28 09:16:41 +0300

    Init commit

---
+++ src/main/java/org/pva/WordsFromWordBruteForce/WordsFromWordBruteForceApplication.java
@@ -0,0 +1,13 @@
+package org.pva.WordsFromWordBruteForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsFromWordBruteForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsFromWordBruteForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pva/WordsFromWordBruteForce/WordsFromWordBruteForceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.WordsFromWordBruteForce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsFromWordBruteForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
