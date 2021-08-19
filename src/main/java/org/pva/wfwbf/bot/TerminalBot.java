a602f599f0252d4db97627ff340164322914ec64
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-08-19 20:09:21 +0300

    Move terminal logic to separate service

--- src/main/java/org/pva/wfwbf/bot/TerminalBot.java
+++ src/main/java/org/pva/wfwbf/bot/TerminalBot.java
@@ -3,30 +3,18 @@ package org.pva.wfwbf.bot;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pva.wfwbf.provider.ParamsProvider;
+import org.pva.wfwbf.service.TerminalService;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
 @AllArgsConstructor
 public class TerminalBot extends TelegramLongPollingBot {
 
+    private final TerminalService terminalService;
     private final ParamsProvider paramsProvider;
-    private static Boolean schedulerStarted = false;
-    private static String lastHealth;
-    private static Date lastHealthDate;
-    private static DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
     @Override
     public String getBotUsername() {
@@ -40,30 +28,6 @@ public class TerminalBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (!schedulerStarted) {
-            ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
-            exec.scheduleAtFixedRate(this::healthCheck, 0, paramsProvider.getTerminalBotWakeUpTimeout(), TimeUnit.SECONDS);
-            schedulerStarted = true;
-        }
-        try {
-            if (lastHealthDate != null && lastHealth != null) {
-                var message = new SendMessage();
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText(String.format("%s %s", df.format(lastHealthDate), lastHealth == null ? "FAIL" : lastHealth));
-                execute(message);
-            }
-        } catch (TelegramApiException e) {
-            log.error(e.toString());
-        }
-    }
-
-    private void healthCheck() {
-        var responseSpec = WebClient.create()
-                .get()
-                .uri(String.format("%s/health", paramsProvider.getTerminalBotWakeUpUrl()))
-                .retrieve();
-        lastHealth = responseSpec.bodyToMono(String.class).block();
-        lastHealthDate = new Date();
-        log.info(lastHealth);
+        terminalService.runCircleHealthCheck();
     }
 }
---
+++ src/main/java/org/pva/wfwbf/service/TerminalService.java
@@ -0,0 +1,48 @@
+package org.pva.wfwbf.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.pva.wfwbf.provider.ParamsProvider;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class TerminalService {
+
+    private final ParamsProvider paramsProvider;
+
+    private static String lastHealth;
+    private static Date lastHealthDate;
+    private static Boolean schedulerStarted = false;
+    private static final DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+    public void runCircleHealthCheck() {
+        if (!schedulerStarted) {
+            ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+            exec.scheduleAtFixedRate(this::healthCheck, 0, paramsProvider.getTerminalBotWakeUpTimeout(), TimeUnit.SECONDS);
+            schedulerStarted = true;
+        }
+        if (lastHealthDate != null && lastHealth != null) {
+            log.info(String.format("%s %s", df.format(lastHealthDate), lastHealth == null ? "FAIL" : lastHealth));
+        }
+    }
+
+    private void healthCheck() {
+        var responseSpec = WebClient.create()
+                .get()
+                .uri(String.format("%s/health", paramsProvider.getTerminalBotWakeUpUrl()))
+                .retrieve();
+        lastHealth = responseSpec.bodyToMono(String.class).block();
+        lastHealthDate = new Date();
+        log.info(lastHealth);
+    }
+}
